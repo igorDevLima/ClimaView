15b9383d38229f4c8de71ba2ceb20721b46e2bf4
Igor Lima <igorlimadrum@gmail.com>
2023-05-18 12:46:05 -0300

    create ChangeUnit component

---
+++ src/components/header/change-unit-buttons/index.tsx
@@ -0,0 +1,16 @@
+const ChangeUnit: React.FC = () => {
+  return (
+    <>
+      <div>
+        <button id="unit-temperature-c">°C</button>
+        <button id="unit-temperature-f">°F</button>
+      </div>
+      <div>
+        <button id="unit-rain-mm">mm</button>
+        <button id="unit-rain-inch">inch</button>
+      </div>
+    </>
+  );
+};
+
+export default ChangeUnit;
