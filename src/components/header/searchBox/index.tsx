4c500eccb2812c4ed0680d7542bbd3b89d39f2e2
Igor Lima <igorlimadrum@gmail.com>
2023-05-18 12:45:37 -0300

    create SeachBox component

---
+++ src/components/header/searchBox/index.tsx
@@ -0,0 +1,9 @@
+const SearchBox: React.FC = () => {
+  return (
+    <>
+      <input type="search" id="search-box" placeholder="Pesquise uma cidade" />
+    </>
+  );
+};
+
+export default SearchBox;
