5372bb3847299dbc282ec04bf1f8266e3ced64cb
Igor Lima <igorlimadrum@gmail.com>
2023-05-20 15:06:03 -0300

    create WeekForecast component

---
+++ src/components/weekForecast/index.tsx
@@ -0,0 +1,140 @@
+const WeekForecast: React.FC = () => {
+  //renderizar em loop
+  return (
+    <div className="forecast-days">
+      <div className="forecast">
+        <div className="img-variable">
+          <div id="icon">
+            <img src="/script/public/img/clear-sky 1.svg" />
+          </div>
+        </div>
+        <div>
+          <span id="day"></span> <span>-</span> <span id="description">Nublado</span>
+        </div>
+        <div>
+          <span id="span-max"></span>
+          <span>max /</span>
+          <span id="span-min"></span>
+          <span>min</span>
+        </div>
+
+        <div>
+          <img src="/script/public/img/pluviometer.svg" />
+          <span id="span-pluviometer"></span>
+          <span id="rain-pluv">mm</span>
+        </div>
+        <div>
+          <img id="umbrella" src="/script/public/img/umbrella.svg" />
+          <span id="span-percent"></span>
+        </div>
+      </div>
+      <div className="forecast">
+        <div className="img-variable">
+          <div id="icon">
+            <img src="/script/public/img/clear-sky 1.svg" />
+          </div>
+        </div>
+        <div>
+          <span id="day"></span> <span>-</span> <span id="description">Nublado</span>
+        </div>
+        <div>
+          <span id="span-max"></span>
+          <span>max /</span>
+          <span id="span-min"></span>
+          <span>min</span>
+        </div>
+
+        <div>
+          <img src="/script/public/img/pluviometer.svg" />
+          <span id="span-pluviometer"></span>
+          <span id="rain-pluv">mm</span>
+        </div>
+        <div>
+          <img id="umbrella" src="/script/public/img/umbrella.svg" />
+          <span id="span-percent"></span>
+        </div>
+      </div>
+      <div className="forecast">
+        <div className="img-variable">
+          <div id="icon">
+            <img src="/script/public/img/clear-sky 1.svg" />
+          </div>
+        </div>
+        <div>
+          <span id="day"></span> <span>-</span> <span id="description">Nublado</span>
+        </div>
+        <div>
+          <span id="span-max"></span>
+          <span>max /</span>
+          <span id="span-min"></span>
+          <span>min</span>
+        </div>
+
+        <div>
+          <img src="/script/public/img/pluviometer.svg" />
+          <span id="span-pluviometer"></span>
+          <span id="rain-pluv">mm</span>
+        </div>
+        <div>
+          <img id="umbrella" src="/script/public/img/umbrella.svg" />
+          <span id="span-percent"></span>
+        </div>
+      </div>
+      <div className="forecast">
+        <div className="img-variable">
+          <div id="icon">
+            <img src="/script/public/img/clear-sky 1.svg" />
+          </div>
+        </div>
+        <div>
+          <span id="day"></span> <span>-</span> <span id="description">Nublado</span>
+        </div>
+
+        <div>
+          <span id="span-max"></span>
+          <span>max /</span>
+          <span id="span-min"></span>
+          <span>min</span>
+        </div>
+
+        <div>
+          <img src="/script/public/img/pluviometer.svg" />
+          <span id="span-pluviometer"></span>
+          <span id="rain-pluv">mm</span>
+        </div>
+        <div>
+          <img id="umbrella" src="/script/public/img/umbrella.svg" />
+          <span id="span-percent"></span>
+        </div>
+      </div>
+      <div className="forecast">
+        <div className="img-variable">
+          <div id="icon">
+            <img src="/script/public/img/clear-sky 1.svg" />
+          </div>
+        </div>
+        <div>
+          <span id="day"></span> <span>-</span> <span id="description">Nublado</span>
+        </div>
+        <div>
+          <span id="span-max"></span>
+          <span>max /</span>
+          <span id="span-min"></span>
+          <span>min</span>
+        </div>
+
+        <div>
+          <img src="/script/public/img/pluviometer.svg" />
+          <span id="span-pluviometer"></span>
+          <span id="rain-pluv">mm</span>
+        </div>
+        <div>
+          <img id="umbrella" src="/script/public/img/umbrella.svg" />
+          <span id="span-percent"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeekForecast;
