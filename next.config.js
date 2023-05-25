e6b51199619831a5d90a51ff493abb4adc7bcfaa
Igor Lima <igorlimadrum@gmail.com>
2023-05-25 18:27:38 -0300

    remove Vanilla-extract bundle config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
+
 /** @type {import('next').NextConfig} */
-
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
-
-const withVanillaExtract = createVanillaExtractPlugin();
-
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = nextConfig;
