2eca8705a325108acc1292c23cf9d4941646f99f
Igor Lima <igorlimadrum@gmail.com>
2023-08-23 13:22:25 -0300

    add Head tag and viewport meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
     </html>
   );
 }
